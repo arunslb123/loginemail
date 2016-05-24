dd111ed30a8731a7875a392e45164d626869a75c
Arun Prakash <arunslb123@gmail.com>
2016-05-25 05:07:59 +1000

    bnhmjnkl

--- getVideoDetails.php
+++ getVideoDetails.php
@@ -1,6 +1,6 @@
 <?php
- header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+//  header("Access-Control-Allow-Origin: *");
+// header("Content-Type: application/json; charset=UTF-8");
 /*
  * Following code will get single product details
  * A product is identified by product id (pid)
