c6e2d05d7facef1dbafe2515437f9b8980cb9841
Arun Prakash <arunslb123@gmail.com>
2016-05-24 17:17:47 +1000

    jk

--- layout/footer.php
+++ layout/footer.php
@@ -1,2 +1,8 @@
+   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/isotope.js"></script>
+    <script src="js/custom.js"></script>
 </body>
 </html>
