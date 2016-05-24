06348d4054b35cecdd975791b8d4d634bda6f0ab
Arun Prakash <arunslb123@gmail.com>
2016-05-25 04:13:23 +1000

    dfd

--- getVideoDetails.php
+++ getVideoDetails.php
@@ -1,22 +1,93 @@
-
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+//  header("Access-Control-Allow-Origin: *");
+// header("Content-Type: application/json; charset=UTF-8");
+/*
+ * Following code will get single product details
+ * A product is identified by product id (pid)
+ */
+ 
+// array for JSON response
 
-$conn = new mysqli("au-cdbr-azure-east-a.cloudapp.net", "b2c31362b2eda6", "ff62019c", "acsm_832eae7fe45825a");
+ 
+// include db connect class
+require('includes/config.php');
 
-$result = $conn->query("SELECT F_Name, Scientific_Name, Season  FROM flowers_info");
+echo "in file";
 
-$outp = "[";
-while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
-    if ($outp != "[") {$outp .= ",";}
-    $outp .= '{"Name":"'  . $rs["F_Name"] . '",';
-    $outp .= '"Scientific_Name":"'   . $rs["Scientific_Name"]        . '",';
-    $outp .= '"Season ":"'. $rs["Season "]     . '"}'; 
+$row = array();
+ 
+
+ 
+// check for post data
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+    echo "check";
+    echo $username;
+ 
+    // get a product from products table
+ //select url,description,duration from urls where userName='arun';
+try{
+
+
+    $result = $db->prepare('SELECT url, description, duration FROM urls WHERE userName = 'arun'');
+			//$result->execute(array('username' => $username));
+			$row = $result->fetch(PDO::FETCH_ASSOC);
+
+
 }
-$outp .="]";
 
-$conn->close();
+			catch(PDOException $e) {
+		    echo '<p class="bg-danger">'.$e->getMessage().'</p>';
+		}
+ 
+    if (!empty($row)) {
 
-echo($outp);
+    	echo "result is not empty";
+        // check for empty result
+        if (mysql_num_rows($row) > 0) {
+ 
+            //$result = mysql_fetch_array($result);
+ 
+            $urldetails= array();
+            $urldetails["url"] = $row["url"];
+            $urldetails["description"] = $row["description"];
+            $urldetails["duration"] = $row["duration"];
+            
+            // success
+            $row["success"] = 1;
+ 
+            // user node
+            $row["urldetails"] = array();
+ 
+            array_push($row["urldetails"], $urldetails);
+ 
+            // echoing JSON response
+            echo json_encode($row);
+        } else {
+            // no product found
+            $row["success"] = 0;
+            $row["message"] = "No Datafound";
+ 
+            // echo no users JSON
+            echo json_encode($row);
+        }
+    } else {
+    	echo "line 59 else";
+        // no product found
+        $row["success"] = 0;
+        $row["message"] = "No Datafound";
+ 
+        // echo no users JSON
+        echo json_encode($row);
+    }
+} else {
+    // required field is missing
+
+    echo "line 70 else";
+    $row["success"] = 0;
+    $row["message"] = "Required field(s) is missing";
+ 
+    // echoing JSON response
+    echo json_encode($row);
+}
 ?>
