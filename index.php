bdb1bac874220e41237e73a703e6585ed9f19531
Arun Prakash <arunslb123@gmail.com>
2016-05-25 22:40:39 +1000

    bam,m.

--- index.php
+++ index.php
@@ -119,7 +119,10 @@ require('layout/header.php');
 
 	<div class="row">
 
-	    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+	    <div class="login-page">
+
+	    <center><h1><span style="color:#a62100"> You</span>Save </h1> </center>
+      <center> <h3>The best place to save your favorite youtube videos!</h3> </center>
 			<form role="form" method="post" action="" autocomplete="off">
 				<h2>Please Sign Up</h2>
 				<p>Already a member? <a href='login.php'>Login</a></p>
