0b3e7241245eb2d6e8f27c0287f9f2a417d97647
Arun Prakash <arunslb123@gmail.com>
2016-05-25 15:54:57 +1000

    dasds

--- getVideoDetails.php
+++ getVideoDetails.php
@@ -12,7 +12,7 @@ header("Content-Type: application/json; charset=UTF-8");
 // include db connect class
 require('includes/config.php');
 
-echo "in file";
+
 
 
  
@@ -21,8 +21,7 @@ echo "in file";
 // check for post data
 if (isset($_SESSION['username'])) {
     $username = $_SESSION['username'];
-    echo "check";
-    echo $username;
+
  
     // get a product from products table
  //select url,description,duration from urls where userName='arun';
@@ -30,26 +29,28 @@ try{
 
 
 
-echo "before result";
 
    //  $result = $db->prepare('SELECT url, description, duration FROM urls where userName = :username');
 			// $result->execute(array('username' => $username));
 			//$row = $result->fetch(PDO::FETCH_ASSOC);
 
 			$stmt = $db->prepare("SELECT url,description,duration FROM urls WHERE userName = :username");
-		   $stmt->execute(array(':username' => 'arun'));
+		   $stmt->execute(array(':username' => $_SESSION['username']));
 		   //$stmt->fetch();
 		//echo $stmt;
 		//$row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-			echo "after result";
-$outp = "[";
+			
+$outputString = "[";
 
 				while($rs = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    if ($outp != "[") {$outp .= ",";}
-    $outp .= '{"title":"'  . $rs["description"] . '",';
-    $outp .= '"url":"'   . $rs["url"]        . '",';
-    $outp .= '"time":"'. $rs["duration"]     . '"}'; 
+    if ($outputString != "[") 
+     {
+     	$outputString .= ",";
+     }
+    $outputString .= '{"title":"'  . $rs["description"] . '",';
+    $outputString .= '"url":"'   . $rs["url"]        . '",';
+    $outputString .= '"time":"'. $rs["duration"]     . '"}'; 
 }
 $outp .="]";
 
