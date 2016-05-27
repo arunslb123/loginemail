379fafdae4c9f64d4773bc9b508110344d21dd91
Arun Prakash <arunslb123@gmail.com>
2016-05-27 15:40:09 +1000

    c

--- home.php
+++ home.php
@@ -197,7 +197,7 @@ require('layout/header.php');
 	</div>
   
     <div class="row">
-      <div class="col-xs-3 col-xs-offset-1">
+      <div class="col-xs-6 col-xs-offset-1">
        <div class="logo"><center><h1><span style="color:#8DE6DC"> You</span> <span style="color:#8FC2F5">Save </h1> </center></span></div>
       </div>
       
