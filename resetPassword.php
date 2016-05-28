2805da345c504bfc8f4ccb1bbece61bd02df813b
Arun Prakash <arunslb123@gmail.com>
2016-05-28 15:20:10 +1000

    ssd

--- resetPassword.php
+++ resetPassword.php
@@ -66,7 +66,7 @@ require('layout/header.php');
 
 <div class="container">
 
-	<div class="row">
+	<div class="row login-page">
 
 	    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
 
