853dce103f9c9d73277f0390b539ef5ea10134e3
Arun Prakash <arunslb123@gmail.com>
2016-05-28 19:17:35 +1000

    hjkm

--- home.php
+++ home.php
@@ -207,20 +207,18 @@ require('layout/header.php');
 
       <form role="form" method="post" action="" autocomplete="off">
       <div class="row">
-      <div class="col-xs-8 col-xs-offset-2 col-lg-5 col-lg-offset-3">
+      <div class="col-xs-6 col-xs-offset-2 big-width">
     <!--   <input type="text" class="form-control" id="youtubelink" placeholder="Enter your favorite youtube video url"> -->
-    
+
     <input type="text" name="youtubelink" id="youtubelink" class="form-control" placeholder="Enter your favorite youtube video url" >
-   
 
 
 
       </div>
-      <div class="col-xs-2 col-lg-2">
+      <div class="col-xs-4">
 
-        
+
         <input type="submit" name="submitvideo" value="Save it" class="btn btn-danger btn-color">
-       
       </div>
       <br><br><br><br>
       </div>
