49540ff9da679b462b21d2171f790ce783d9353a
Arun Prakash <arunslb123@gmail.com>
2016-05-28 19:41:40 +1000

    dfdf

--- home.php
+++ home.php
@@ -207,7 +207,7 @@ require('layout/header.php');
 
       <form role="form" method="post" action="" autocomplete="off">
       <div class="row">
-      <div class="col-xs-9 col-lg-5 col-lg-offset-2 big-width">
+      <div class="col-xs-9 col-lg-5 col-lg-offset-3 big-width">
     <!--   <input type="text" class="form-control" id="youtubelink" placeholder="Enter your favorite youtube video url"> -->
 
     <input type="text" name="youtubelink" id="youtubelink" class="form-control" placeholder="Enter your favorite youtube video url" >
