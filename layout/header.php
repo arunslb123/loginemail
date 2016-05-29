5b5f373cb1cfb97f46c0c7c2c063ba883fd6f6aa
Arun Prakash <arunslb123@gmail.com>
2016-05-29 16:15:29 +1000

    hh

--- layout/header.php
+++ layout/header.php
@@ -5,6 +5,7 @@
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
     <meta name="viewport" content="width=device-width" />
+    <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title><?php if(isset($title)){ echo $title; }?></title>
 
