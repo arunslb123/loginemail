b91d48642726c678b22db7c1f765c9384b44ae9e
Arun Prakash <arunslb123@gmail.com>
2016-05-29 23:35:06 +1000

    jaj

--- home.php
+++ home.php
@@ -177,7 +177,7 @@ require('layout/header.php');
         <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
           <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
         </a>
-        <div class="scale-media" id="trailer-video-container">
+        <div class="scale-media videowrapper" id="trailer-video-container">
         </div>
       </div>
     </div>
